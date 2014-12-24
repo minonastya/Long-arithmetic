cae3fe23aaf2199dada2c734c3966a6deec65410
minonastya <minovskayanastya@gmail.com>
2014-12-24 12:31:12 +0300

    commit

---
+++ longarith.h
@@ -0,0 +1,5 @@
+#include "longnumb.h"
+void longNum_addition(number **num1, number **num2, number **res);
+void longNum_substraction(number **num1, number **num2, number **res);
+void longNum_multiplication(number **num1, number **num2, number **res);
+void longNum_division(number **num1, number **num2, number **res);
